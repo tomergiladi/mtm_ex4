613fa89b8b877a9c1e1133790f1bcbff78741dc2
you@example.com <you@example.com>
2018-06-10 21:47:55 +0300

    bla

--- Player.cpp
+++ Player.cpp
@@ -20,7 +20,7 @@ Player::Player(const Player& player)
     strcpy(this->name, player.name);
 }
 Player& Player::operator=(const Player& player) {
-    // we dont want to change anything if we are assigning to the same player
+    // we dont want to change anything if we are assigning to the same playeraaa
     if (this == &player) {
         return *this;
     }
